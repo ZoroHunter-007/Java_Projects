16b4917de697bdb4e7644f210edc24df15daec52
Dhruv <dhruvmalusare13@gmail.com>
2025-11-27 13:07:44 +0530

    Added all Programs

--- SpringBootDev/src/main/java/com/example/SpringBootDev/BankService.java
+++ SpringBootDev/src/main/java/com/example/SpringBootDev/BankService.java
@@ -1,5 +1,7 @@
 package com.example.SpringBootDev;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -12,5 +14,7 @@ public class BankService {
 	public Bank SaveBankData(Bank b) {
 		return bankrepository.saveAndFlush(b);
 	}
-	
+	public List<Bank> FindAllData(){
+		return bankrepository.findAll();
+	}
 }
