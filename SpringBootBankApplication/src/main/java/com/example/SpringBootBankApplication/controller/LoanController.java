d853422bf6712566537db1b20ed464910eb586a0
Dhruv <dhruvmalusare13@gmail.com>
2025-12-04 14:26:05 +0530

    Added All Programs

---
+++ SpringBootBankApplication/src/main/java/com/example/SpringBootBankApplication/controller/LoanController.java
@@ -0,0 +1,5 @@
+package com.example.SpringBootBankApplication.controller;
+
+public class LoanController {
+
+}
---
+++ SpringBootBankApplication/src/main/java/com/example/SpringBootBankApplication/entity/Account.java
@@ -0,0 +1,5 @@
+package com.example.SpringBootBankApplication.entity;
+
+public class Account {
+
+}
